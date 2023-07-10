6d935c14499246b9fbbe9dd06b4ff2d5ad3b0fd3
fiendskull9 <fiendskull9@gmail.com>
2023-07-09 23:08:45 -0500

    chamges
    cham

---
+++ shows.js
@@ -0,0 +1,41 @@
+$(function() {
+		
+	$.get( "file.xml", function( data ) {
+    let xml = new XMLSerializer().serializeToString(data);
+	
+
+		let dataraw = $.parseXML(xml); 
+	
+		let $xml = $(dataraw);
+	
+		let $show = $xml.find("event");
+	
+		let content = "";
+		$show.each(function(){
+		
+			let showdate = $(this).find('date').text();
+			let venue = $(this).find('venue').text();
+			let showlocation = $(this).find('location').text();
+			let starttime = $(this).find('timestart').text();
+			let endtime = $(this).find('timeend').text();
+
+			
+			content += `<div class="row" id="show">`
+				+ `	<div class="col-4" id="show-date">`+showdate+`</div>`
+				+ `	<div class="col-8" id="show-info">`
+				+ ` <h1>`+showlocation+`</h1>`
+				+ `	<h2>`+venue+`</h2>`
+				+ `	<i>`+starttime+` - `+endtime+`</i>`
+				+ `<br> <a class="link" href="`+eventlink+`" target="_blank">
+									<i class="fab fa-facebook">&nbsp;</i>Event Link
+								</a>`
+				+ `</div>`
+				+ `</div>`;
+			
+			$("#shows" ).html(content);
+		
+		});
+	});
+	
+	
+});
