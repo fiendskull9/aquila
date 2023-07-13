10cbf3bd0a0f541d3dae42e7021a8fe56bcb5582
fiendskull9 <fiendskull9@gmail.com>
2023-07-12 21:24:16 -0500

    ya

--- shows.js
+++ shows.js
@@ -1,17 +1,16 @@
 $(function() {
-		
-	
-$.ajax({
-type: "GET",
-url: "shows.xml",
-dataType: "xml",
-success: function(xmlraw) {
- //remainder of the code
+        var xmlhttp = new XMLHttpRequest();
+ 
+        xmlhttp.open("GET", "shows.xml", true);
+        xmlhttp.send();
 
-		let dataraw = $.parseXML(xmlraw); 
-	
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+
+		var dataraw = $.parseXML(xmlhttp.response); 
+
+
 		let $xml = $(dataraw);
-	
 		let $show = $xml.find("event");
 	
 		let content = "";
@@ -22,24 +21,35 @@ success: function(xmlraw) {
 			let showlocation = $(this).find('location').text();
 			let starttime = $(this).find('timestart').text();
 			let endtime = $(this).find('timeend').text();
+			let eventlink = $(this).find('eventlink').text();
 
 			
-			content += `<div class="row" id="show">`
-				+ `	<div class="col-4" id="show-date">`+showdate+`</div>`
-				+ `	<div class="col-8" id="show-info">`
-				+ ` <h1>`+showlocation+`</h1>`
-				+ `	<h2>`+venue+`</h2>`
-				+ `	<i>`+starttime+` - `+endtime+`</i>`
-				+ `<br> <a class="link" href="`+eventlink+`" target="_blank">
-									<i class="fab fa-facebook">&nbsp;</i>Event Link
-								</a>`
-				+ `</div>`
-				+ `</div>`;
+			content = `<div class="row"
+     id="show">`
+				+ `	<div class="col"
+	     id="show-date"><h1>`+showdate+`</h1>`
+		 			
+				+ `	</div></div><div class="row" id="show"><div class="col"
+	     id="show-info">`
+				+ `		<strong>`+showlocation+`</strong>`
+				+ `		`+venue+``
+				+ `<br>`+starttime+` - `+endtime+``
+				+ `		<br>
+			<a class="link"
+			   href="`+eventlink+`"
+			   target="_blank">
+				<i class="fab fa-facebook">&nbsp;</i>Event Link
+			</a>`
+				+ `		</div>`
+				+ `	</div>`;
 			
-			$("#shows" ).append(content);
-		
+			$("#shows").append(content);
+			
+		console.log(showdate);
 		});
-	
-	}
+		
+		};
+	};
 });
-});
+
+
